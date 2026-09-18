07ae1678079054751c6d938fa844850a3545ea5c
ahmedinternist <164291282+ahmedinternist@users.noreply.github.com>
2026-09-18 11:24:40 +0300

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: 'sans-serif' }}>
+      <h1>Rx Share</h1>
+      <p>POST prescription JSON to /api/rx to get a shareable link.</p>
+    </main>
+  );
+}
